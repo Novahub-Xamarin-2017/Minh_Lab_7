6acb5c66f48422ae3e0d6d515c8839fdcfdb82f5
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-22 14:56:14 +0700

    Add Exercise01

---
+++ Exercise01/Model/CreateData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01.Model
+{
+    class CreateData
+    {
+        private List<Student> list;
+
+        public CreateData()
+        {
+            list = new List<Student>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                list.Add(new Student { Name = $"{(char)(i + 'a' - 1)}", Score = i, Birthday = new DateTime(1109 + i, 11, 1) });
+            }
+        }
+
+        public List<Student> GetList()
+        {
+            return list;
+        }
+    }
+}
---
+++ Exercise01/Program.cs
@@ -0,0 +1,53 @@
+﻿using Exercise01.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = (new CreateData()).GetList();
+            list.ForEach(Console.WriteLine);
+            Console.WriteLine();
+
+            var newList = list.Where(x => x.Score >= 8).OrderBy(x => x.Birthday).ToList();
+            if (newList.Any())
+            {
+                newList.ForEach(Console.WriteLine);
+            }
+            else
+            {
+                Console.WriteLine("List is empty");
+            }
+            Console.WriteLine();
+
+            newList = list.Where(x => x.Score >= 5 && x.Score <= 7.5).OrderBy(x => x.Name).ToList();
+            if (newList.Any())
+            {
+                newList.ForEach(Console.WriteLine);
+            }
+            else
+            {
+                Console.WriteLine("List is empty");
+            }
+            Console.WriteLine();
+
+            newList = list.Where(x => x.Score >= 6 && x.Score <= 8 && x.Birthday.Year >= 1110 && x.Birthday.Year <= 1112).ToList();
+            if (newList.Any())
+            {
+                newList.ForEach(Console.WriteLine);
+            } else
+            {
+                Console.WriteLine("List is empty");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
