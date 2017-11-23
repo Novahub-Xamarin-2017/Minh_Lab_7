cb0fe89e7d522870c5f8bea597cf5cd2f5404262
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-23 18:15:49 +0700

    Refactor code

--- Exercise08/Program.cs
+++ Exercise08/Program.cs
@@ -10,63 +10,93 @@ namespace Exercise08
     {
         static void Main(string[] args)
         {
-            var n = (Console.ReadLine().Any()) ? int.Parse(Console.ReadLine()) : 0;
+            var nString = Console.ReadLine();
+            var n = 0;
+            int.TryParse(nString, out n);
 
-            var listFirst = new[] { 1, 2, 3, 4, 5, 6 }.ToList();
-            var listSecond = new[] { 4, 5, 6, 7, 8, 9 }.ToList();
+            var numbers1 = new List<int>();
 
-            if (n!=0)
+            if (n != 0)
             {
-                listFirst.Clear();
-                listSecond.Clear();
-
                 for (int i = 1; i <= n; i++)
                 {
-                    var temp = int.Parse(Console.ReadLine());
-                    listFirst.Add(temp);
+                    var tempString = Console.ReadLine();
+                    var temp = 0;
+                    int.TryParse(tempString, out temp);
+                    numbers1.Add(temp);
                 }
+            } else
+            {
+                numbers1 = new[] { 1, 2, 3, 4, 5, 6 }.ToList();
+            }
 
-                n = int.Parse(Console.ReadLine());
+            var mString = Console.ReadLine();
+            var m = 0;
+            int.TryParse(nString, out m);
 
-                for (int i = 1; i <= n; i++)
+            var listSecond = new List<int>();
+
+            if (m != 0)
+            {
+                for (int i = 1; i <= m; i++)
                 {
-                    var temp = int.Parse(Console.ReadLine());
+                    var tempString = Console.ReadLine();
+                    var temp = 0;
+                    int.TryParse(tempString, out temp);
                     listSecond.Add(temp);
                 }
             }
+            else
+            {
+                listSecond = new[] { 4, 5, 6, 7, 8, 9 }.ToList();
+            }
 
-            var listCommon = listFirst.Join(
+            var listCommon = numbers1.Join(
                 listSecond,
                 x => x,
                 y => y,
                 (x, y) => new
+                    {
+                        x = x
+                    })
+                .ToList();
+
+            listCommon.ForEach(Console.WriteLine);
+            Console.WriteLine();
+
+            var listOfNumberOnlyExistListFirst = numbers1
+                .Where(x => !listSecond
+                    .Any(y => y == x))
+                .ToList();
+
+            listOfNumberOnlyExistListFirst.ForEach(Console.WriteLine);
+            Console.WriteLine();
+
+            var listOfNumberExistOnlyList = numbers1
+                .Union(listSecond)
+                .Where(x => !listCommon
+                    .Any(y => y.x == x))
+                .ToList();
+
+            listOfNumberExistOnlyList.ForEach(Console.WriteLine);
+            Console.WriteLine();
+
+
+            var sum = numbers1.Select((x, i) => new
                 {
-                    x = x
-                });
-            foreach (var i in listCommon)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine();
-
-            var listOfNumberOnlyExistListFirst = listFirst.Where(x => !listSecond.Any(y => y == x));
-            foreach (var i in listOfNumberOnlyExistListFirst)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine();
-
-            var listOfNumberExistOnlyList = listFirst.Union(listSecond).Where(x => !listCommon.Any(y => y.x == x));
-            foreach (var i in listOfNumberExistOnlyList)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine();
-
-
-            var sum = listFirst.Select((x, i) => new { x = x, i = i }).TakeWhile(x => true)
-                .Sum(x => listSecond.Select((y, j) => new { y = y, j = j }).TakeWhile(y => true)
+                    x = x,
+                    i = i
+                })
+                .TakeWhile(x => true)
+                .Sum(x => listSecond
+                .Select((y, j) => new
+                    {
+                        y = y,
+                        j = j
+                    })
+                .TakeWhile(y => true)
                 .Sum(y => (y.j != x.i) ? (y.y * x.x) : 0));
+
             Console.WriteLine(sum);
 
             Console.ReadKey();
