a8a09cec2b10211b1528a3b0248855dded7d33ac
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-23 16:43:07 +0700

    Refactor code

---
+++ Exercise01/Model/StudentGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01.Model
+{
+    class StudentGenerator
+    {
+        public List<Student> CreateStudents()
+        {
+            var students = new List<Student>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                var student = new Student { Name = $"{(char)(i + 'a' - 1)}", Score = i, Birthday = new DateTime(1109 + i, 11, 1) };
+                students.Add(student);
+            }
+
+            return students;
+        }
+    }
+}
--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -11,14 +11,14 @@ namespace Exercise01
     {
         static void Main(string[] args)
         {
-            var list = (new CreateData()).GetList();
+            var list = (new StudentGenerator()).CreateStudents();
             list.ForEach(Console.WriteLine);
             Console.WriteLine();
 
-            var newList = list.Where(x => x.Score >= 8).OrderBy(x => x.Birthday).ToList();
-            if (newList.Any())
+            var goodStudents = list.Where(x => x.Score >= 8).OrderBy(x => x.Birthday).ToList();
+            if (goodStudents.Any())
             {
-                newList.ForEach(Console.WriteLine);
+                goodStudents.ForEach(Console.WriteLine);
             }
             else
             {
@@ -26,10 +26,10 @@ namespace Exercise01
             }
             Console.WriteLine();
 
-            newList = list.Where(x => x.Score >= 5 && x.Score <= 7.5).OrderBy(x => x.Name).ToList();
-            if (newList.Any())
+            var studentsHasScoreBetWeen5And7 = list.Where(x => x.Score >= 5 && x.Score <= 7.5).OrderBy(x => x.Name).ToList();
+            if (studentsHasScoreBetWeen5And7.Any())
             {
-                newList.ForEach(Console.WriteLine);
+                studentsHasScoreBetWeen5And7.ForEach(Console.WriteLine);
             }
             else
             {
@@ -37,10 +37,10 @@ namespace Exercise01
             }
             Console.WriteLine();
 
-            newList = list.Where(x => x.Score >= 6 && x.Score <= 8 && x.Birthday.Year >= 1110 && x.Birthday.Year <= 1112).ToList();
-            if (newList.Any())
+            var studentsHasScoreBetWeen6And8 = list.Where(x => x.Score >= 6 && x.Score <= 8 && x.Birthday.Year >= 1110 && x.Birthday.Year <= 1112).ToList();
+            if (studentsHasScoreBetWeen6And8.Any())
             {
-                newList.ForEach(Console.WriteLine);
+                studentsHasScoreBetWeen6And8.ForEach(Console.WriteLine);
             } else
             {
                 Console.WriteLine("List is empty");
