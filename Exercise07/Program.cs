2e7e3233fff5d9375083b17f3c36bd51cd16e933
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-23 10:53:02 +0700

    Add Exercise07

---
+++ Exercise07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = (Console.ReadLine().Any()) ? int.Parse(Console.ReadLine()) : 5;
+            var m = (Console.ReadLine().Any()) ? int.Parse(Console.ReadLine()) : 20;
+
+            var ints = Enumerable.Range(1, m-1);
+            var list = ints.OrderBy(x => Guid.NewGuid()).Take(n);
+
+            foreach (var i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
