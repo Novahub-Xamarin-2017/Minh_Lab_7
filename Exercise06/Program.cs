503462183d222d153c2aaa9b87d12110a5afe767
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-23 10:40:45 +0700

    Add Exercise06

---
+++ Exercise06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 100;
+
+            var ints = Enumerable.Range(2, n);
+            var list = ints.TakeWhile(x => x < n)
+                .Where(x => !ints.TakeWhile(y => y <= (int)Math.Sqrt(x))
+                .Any(y => x % y == 0));
+
+            foreach(var i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
