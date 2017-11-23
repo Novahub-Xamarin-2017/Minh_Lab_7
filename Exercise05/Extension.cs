d9b1760583d981850ffea680debfa3b3643be1d0
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-23 10:05:13 +0700

    Add Exercise05

---
+++ Exercise05/Extension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise05
+{
+    public static class Extension
+    {
+        public static IEnumerable<List<T>> SubList<T>(this IList<T> list, int max)
+        {
+            var cnt = 0;
+            var newList = new List<T>();
+
+            foreach (var i in list)
+            {
+                newList.Add(i);
+                cnt++;
+                if (cnt == max || i.Equals(list.Last())) 
+                {
+                    yield return newList;
+                    cnt = 0;
+                    newList.Clear();
+                }
+            }
+        }
+    }
+}
---
+++ Exercise05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise05
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            var list = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }.SubList(4);
+
+            foreach(var i in list)
+            {
+                foreach (var j in i)
+                {
+                    Console.WriteLine(j);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
