7df54746455f88d7bfdc3588bd63c82eb0e0552d
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-25 11:12:09 +0700

    Refactor code

--- Exercise08/Program.cs
+++ Exercise08/Program.cs
@@ -10,25 +10,21 @@ namespace Exercise08
     {
         static void Main(string[] args)
         {
-            var numbers1 = inputNumbers(new[] { 1, 2, 3, 4, 5, 6 }).ToList();
-            var numbers2 = inputNumbers(new[] { 4, 5, 6, 7, 8, 9 }).ToList();
+            var numbers1 = new[] { 1, 2, 3, 4, 5, 6 }.ToList();
+            var numbers2 = new[] { 4, 5, 6, 7, 8, 9 }.ToList();
 
             var listCommon = numbers1.Join(
                 numbers2,
                 x => x,
                 y => y,
-                (x, y) => new
-                    {
-                        x = x
-                    })
+                (x, y) => x)
                 .ToList();
 
             listCommon.ForEach(Console.WriteLine);
             Console.WriteLine();
 
             var listOfNumberOnlyExistListFirst = numbers1
-                .Where(x => !numbers2
-                    .Any(y => y == x))
+                .Where(x => numbers2.All(y => y != x))
                 .ToList();
 
             listOfNumberOnlyExistListFirst.ForEach(Console.WriteLine);
@@ -36,61 +32,19 @@ namespace Exercise08
 
             var listOfNumberExistOnlyList = numbers1
                 .Union(numbers2)
-                .Where(x => !listCommon
-                    .Any(y => y.x == x))
+                .Where(x => listCommon.All(y => y != x))
                 .ToList();
 
             listOfNumberExistOnlyList.ForEach(Console.WriteLine);
             Console.WriteLine();
 
-
-            var sum = numbers1.Select((x, i) => new
-                {
-                    x = x,
-                    i = i
-                })
-                .TakeWhile(x => true)
-                .Sum(x => numbers2
-                .Select((y, j) => new
-                    {
-                        y = y,
-                        j = j
-                    })
-                .TakeWhile(y => true)
-                .Sum(y => (y.j != x.i) ? (y.y * x.x) : 0));
+            var sum = Enumerable.Range(0, numbers1.Count)
+                .Sum(x => Enumerable.Range(0, numbers2.Count)
+                    .Sum(y => (y != x) ? (numbers1[x] * numbers2[y]) : 0));
 
             Console.WriteLine(sum);
 
             Console.ReadKey();
-        }
-
-        static IEnumerable<int> inputNumbers(IList<int> defaultNumbers)
-        {
-            var n = InputNumber(Console.ReadLine(), 0);
-
-            if (n != 0)
-            {
-                for (int i = 1; i <= n; i++)
-                {
-                    var temp = InputNumber(Console.ReadLine(), 0);
-                    yield return temp;
-                }
-            }
-            else
-            {
-                foreach (var defaultNumber in defaultNumbers)
-                {
-                    yield return defaultNumber;
-                }
-            }
-            
-        }
-
-        static int InputNumber(string str, int defaultNumber)
-        {
-            var number = 0;
-            int.TryParse(str, out number);
-            return (number == 0) ? defaultNumber : number;
         }
     }
 }
